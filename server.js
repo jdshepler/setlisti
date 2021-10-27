c2536680c36e24cc1b835c178f7197cd6977ca45
jdshepler <jshepler@umflint.edu>
2021-10-27 09:57:33 -0400

    Heroku deployment

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.use("/setlist/", require("./apis/setlistAPI"));
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.get("/authenticate", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/authenticate.html"));
 });
 
 app
@@ -117,7 +117,7 @@ app.get("/callback", function (req, res) {
 
         // we can also pass the token to the browser to make requests from there
         res.redirect(
-          "http://localhost:3000/#" +
+          "https://setlisti.herokuapp.com/#" +
             querystring.stringify({
               access_token: access_token,
               refresh_token: refresh_token,
@@ -160,6 +160,10 @@ app.get("/refresh_token", function (req, res) {
       });
     }
   });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
