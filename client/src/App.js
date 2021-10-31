662563b8eae0d7381fa6f201b03958638c467eef
jdshepler <jshepler@umflint.edu>
2021-10-31 18:12:51 -0400

    Heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -286,7 +286,6 @@ class App extends Component {
               autoFocus
               defaultValue={this.state.artistName}
               onChange={this.onChangeArtist}
-              onKeyPress={this.searchArtist}
             />
           </Grid>
           <Grid item>
